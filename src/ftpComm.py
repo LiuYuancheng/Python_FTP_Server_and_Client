e9fca6c82175efae4be50540f448c19bbe108d02
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2024-07-25 16:52:03 +0800

    Added the main function for the ftpComm lib module.

--- src/ftpComm.py
+++ src/ftpComm.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python
 #-----------------------------------------------------------------------------
-# Name:        ftpServer.py
+# Name:        ftpComm.py
 #
-# Purpose:     This module will provide a FTP server which can run in parallel 
-#              thread for file transfer.
+# Purpose:     This lib module will provide FTP Communication client and server
+#              class for integrate in other program for file trafer.
 # 
 # Author:      Yuancheng Liu
 #
 # Created:     2024/07/23
-# Version:     v_0.0.1
+# Version:     v_0.1.1
 # Copyright:   Copyright (c) 2024 LiuYuancheng
 # License:     MIT License
 #-----------------------------------------------------------------------------
+""" Liborary Design:
+
+    This lib module will provide FTP Communication client and server class for 
+    integrate in other program for file trafer.
+    - The server will use the pyftpdlib: https://pypi.org/project/pyftpdlib/
+    - The client will use the ftplib: https://docs.python.org/3/library/ftplib.html
+"""
 
 import os
-
 from ftplib import FTP
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler, ThrottledDTPHandler
 from pyftpdlib.servers import FTPServer, ThreadedFTPServer  
-DEF_FTP_PORT = 8081
-DEF_PERM = 'elradfmwM'  # Default permission for user
 
+# Default FTP connection port
+DEF_FTP_PORT = 8081
+
+DEF_PERM = 'elradfmwM'  # Default permission for user
 # Read permissions:
 # "e" = change directory (CWD, CDUP commands)
 # "l" = list files (LIST, NLST, STAT, MLSD, MLST, SIZE commands)
@@ -34,29 +42,41 @@ DEF_PERM = 'elradfmwM'  # Default permission for user
 # "w" = store a file to the server (STOR, STOU commands)
 # "M" = change file mode / permission (SITE CHMOD command) New in 0.7.0
 # "T" = change file modification time (SITE MFMT command) New in 1.5.3
-DIR_PATH = dirpath = os.path.dirname(os.path.abspath(__file__))
+
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 DEF_USER = {
     'admin': {
         'passwd': '123456',
         'perm': DEF_PERM,
-        'dirpath': os.path.join(dirpath, 'ftpServer_data')
+        'dirpath': os.path.join(DIR_PATH, 'ftpServer_data')
     }
 }
-
+# default max speed for client download
 DEF_READ_MAX_SPEED = 300 * 1024  # 300 Kb/sec (30 * 1024)
+# default max speed for client upload
 DEF_WRITE_MAX_SPEED = 300 * 1024  # 300 Kb/sec (30 * 1024)
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 class ftpServer(object):
-    
     """ FTP server class."""
     def __init__(self, rootDirPath, port=DEF_FTP_PORT, userDict=DEF_USER,
                  readMaxSp=DEF_READ_MAX_SPEED, writeMaxSp=DEF_WRITE_MAX_SPEED,
                  threadFlg=False):
+        """ Init example:
+            ftpServer('/', port=8081, userDict={'user':{'passwd':'123456','perm':ftpComm.DEF_FTP_PORT,'dirpath':'/home/user'}},
+                readMaxSp=100000, writeMaxSp=100000,)
+        Args:
+            rootDirPath (str): The FTP host file storage root foler.
+            port (int, optional): FTP server port. Defaults to DEF_FTP_PORT.
+            userDict (dict, optional): Avaliable user dictionary. Defaults to DEF_USER.
+            readMaxSp (int, optional): FTP client max allowed download speed(Kb). Defaults to DEF_READ_MAX_SPEED.
+            writeMaxSp (_type_, optional): FTP client max allowed upload speed(Kb). Defaults to DEF_WRITE_MAX_SPEED.
+            threadFlg (bool, optional): flag to identify whether can run the server in subthread. Defaults to False.
+        """
+        # Init the server basic parameters
         self._port = int(port)
-        if not os.path.exists(rootDirPath):
-            os.makedirs(rootDirPath)
+        if not os.path.exists(rootDirPath): os.makedirs(rootDirPath)
         self._rootPath = rootDirPath
         self._user = userDict
         self._threadFlg = threadFlg
@@ -64,7 +84,8 @@ class ftpServer(object):
         # Instantiate a dummy authorizer for managing 'virtual' users
         self.authorizer = DummyAuthorizer()
         self._initAuthorization()
-
+        
+        # Initiate the throttled DTP handler class for uplad/download speed control
         self.dtphandler = ThrottledDTPHandler
         self.dtphandler.read_limit = int(readMaxSp)
         self.dtphandler.write_limit = int(writeMaxSp)
@@ -74,20 +95,21 @@ class ftpServer(object):
         self.handler.authorizer = self.authorizer
         self.handler.dtp_handler = self.dtphandler
 
-        # Define a customized banner (string returned when client connects)
+        # Define a customized banner (welcome string returned when client connects)
         self.handler.banner = "FTP server ready, license port: %s" % str(self._port)
 
+        # Init the FTP server.
         address = ('0.0.0.0', self._port)
         self.server = None
         if self._threadFlg:
             self.server = ThreadedFTPServer(address, self.handler)
         else:
             self.server = FTPServer(address, self.handler)
-        print("FTP server started on port: %s" % str(self._port))
+        print("FTP server started to host on port: %s" % str(self._port))
 
     #-----------------------------------------------------------------------------
     def _initAuthorization(self):
-        """ Initialize the user authorization."""
+        """ Initialize the user authorization and add user to the authorizer."""
         for user, info in self._user.items():
             userDir = info['dirpath'] if 'dirpath' in info.keys() else self._rootPath
             self.authorizer.add_user(user, info['passwd'], userDir, perm=info['perm'])
@@ -95,25 +117,22 @@ class ftpServer(object):
 
     #-----------------------------------------------------------------------------
     def addUser(self, user, passwd, dirpath=None, perm='elradfmwM'):
-        """ Add a new user to the server."""
+        """ Add a new user to the server.
+            Args:
+                user (str): unique user name.
+                passwd (str): user login password.
+                dirpath (path, optional): user own home dir under root dir. Defaults to None.
+                perm (str, optional): permission. Defaults to 'elradfmwM'.
+            Returns:
+                bool: True if use is added successful, False if user already exists or add failed.
+        """
         if user in self._user.keys():
-            print("User %s already exists" % user)
+            print("addUser(): User %s already exists" % user)
             return False
         self._user[user] = {'passwd': passwd, 'dirpath': dirpath, 'perm': perm}
         if dirpath is None: dirpath = self._rootPath
         self.authorizer.add_user(user, passwd, dirpath, perm=perm)
-        print("User %s added" % user)
-        return True
-
-    #-----------------------------------------------------------------------------
-    def removeUser(self, user):
-        """ Remove a user from the server."""
-        if user not in self._user.keys():
-            print("User %s does not exist" % user)
-            return False
-        self.authorizer.remove_user(user)
-        del self._user[user]
-        print("User %s removed" % user)
+        print("addUser(): User [%s] is added." % user)
         return True
 
     #-----------------------------------------------------------------------------
@@ -121,44 +140,77 @@ class ftpServer(object):
         return self._user
 
     #-----------------------------------------------------------------------------
+    def removeUser(self, userName):
+        """ Remove a user from the server authorizer."""
+        if userName not in self._user.keys():
+            print("removeUser(): User %s does not exist" % userName)
+            return False
+        self.authorizer.remove_user(userName)
+        del self._user[userName]
+        print("removeUser(): User %s removed" % userName)
+        return True
+    
+    #-----------------------------------------------------------------------------
     def startServer(self):
         """ Start the FTP server."""
         print("Starting FTP server...")
-        if self.server is not None:
-            self.server.serve_forever()
+        if self.server is not None: self.server.serve_forever()
         print("FTF server stopped")
 
     #-----------------------------------------------------------------------------
     def stopServer(self):
         """ Stop the FTP server."""
         print("Stopping FTP server...")
-        if self.server is not None:
-            self.server.close_all()
+        if self.server is not None: self.server.close_all()
         print("FTF server stopped")
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 class ftpClient(object):
-    """ FTP client class for file transfer. """
+    """ FTP client class for file transfer."""
 
     def __init__(self, host, port, user, pwd):
+        """ Init example:
+            client = ftpComm.ftpClient('127.0.0.1', 8081, 'admin', '123456')
+            Args:
+                host (str): ftp server ip address.
+                port (int): ftp server port.
+                user (str): user name to login the ftp server.
+                pwd (str): password to login the ftp server.
+        """
         self.host = host
         self.port = port
         self.user = user
         self.pwd = pwd
         self.client = FTP()
         self.connected = False
+        print("Ftp client inited.")
 
+    #-----------------------------------------------------------------------------
     def connectToServer(self):
         """ Connect to FTP server. """
-        self.client.connect(self.host, self.port)
-        self.client.login(self.user, self.pwd)
-        print(self.client.getwelcome())
-        return True
+        try:
+            self.client.connect(self.host, self.port)
+            self.client.login(self.user, self.pwd)
+            print(self.client.getwelcome())
+            self.connected = True
+            return True
+        except Exception as err:
+            print("ftpClient() connect to server Error: %s" %str(err))
+            self.connected = False
+            return False
 
+    #-----------------------------------------------------------------------------
     def createDir(self, dirname):
+        """ Create directory on FTP server under current directory."""
         self.client.mkd(dirname)
 
+    #-----------------------------------------------------------------------------
+    def swithToDir(self, dir):
+        """ Switch to the target directory. """
+        self.client.cwd(dir)
+
+    #-----------------------------------------------------------------------------
     def listDirInfo(self, detail=False):
         """ List directory information. """
         if detail:
@@ -166,13 +218,18 @@ class ftpClient(object):
         else:
             return self.client.nlst()
 
-
-    def swithToDir(self, dir):
-        """ Switch to the target directory. """
-        self.client.cwd(dir)
-
+    #-----------------------------------------------------------------------------
     def uploadFile(self, localFile, remoteFile):
-        """ Upload file to FTP server. """
+        """ Upload local file to FTP server side's current directory.
+            Args:
+                localFile (str): local file path.
+                remoteFile (str): remote file name.
+            Returns:
+                bool: True if upload successful, else false.
+        """
+        if not os.path.exists(localFile):
+            print("Error: uploadFile()> local file not exist: %s" %str(localFile))
+            return False
         try:
             self.client.storbinary('STOR ' + remoteFile, open(localFile, 'rb'))
             return True
@@ -180,8 +237,9 @@ class ftpClient(object):
             print("ftpClient() upload file Error: %s" %str(err))
             return False
 
+    #-----------------------------------------------------------------------------
     def downloadFile(self, remoteFile, localFile):
-        """ Download file from FTP server. """
+        """ Download file from FTP server side's current directory to local. """
         try:
             self.client.retrbinary('RETR ' + remoteFile, open(localFile, 'wb').write)
             return True
@@ -189,6 +247,49 @@ class ftpClient(object):
             print("ftpClient() download file Error: %s" %str(err))
             return False
 
+    #-----------------------------------------------------------------------------
     def close(self):
         self.client.quit()
 
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+def main():
+    print("Input the program you want to run: [1]FTP Client [2]FTP Server")
+    choice = int(input())
+    if choice == 1:
+        print("Init FTP Client")
+        serverIP = input("Input Connected server IP:")
+        serverPort = int(input("Input Connected server port:"))
+        userName = input("Input Connected server username:")
+        password = input("Input Connected server password:")
+        client = ftpClient(serverIP, serverPort, userName, password)
+        client.connectToServer()
+        while True:
+            print("Input the program you want to run:[0]Swith to Dir [1]Upload [2]Download [3]List dir [4]exit")
+            choice = int(input())
+            if choice == 0:
+                dir = input("Input dir:")
+                client.swithToDir(dir)
+            elif choice == 1:
+                localFile = input("Input local file path:")
+                remoteFile = input("Input remote file name:")
+                client.uploadFile(localFile, remoteFile)
+            elif choice == 2:
+                remoteFile = input("Input remote file name:")
+                localFile = input("Input local file path:")
+                client.downloadFile(remoteFile, localFile)
+            elif choice == 3:
+                print(client.listDirInfo(detail=True))
+            else:
+                break
+        client.close()
+    elif choice == 2:
+        print("Init FTP Server")
+        serverPort = int(input("Input Connected server port:"))
+        dir = os.path.join(DIR_PATH, 'ftpServer_data')
+        server = ftpServer(dir, port=serverPort, threadFlg=True)
+        server.startServer()
+
+#-----------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
