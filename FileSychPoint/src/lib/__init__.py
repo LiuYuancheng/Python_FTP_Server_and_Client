f309e0967c04f663c91a99964ac31bf15ecb618f
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2025-02-03 16:40:30 +0800

    Added file share point server lib file.

---
+++ FileSychPoint/src/lib/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Name:        __init__.py
+#
+# Purpose:     The regular package init module to init the customer "lib".
+#
+# Author:      Yuancheng Liu
+#
+# Created:     2014/01/15
+# Copyright:   Copyright (c) 2024 LiuYuancheng
+# License:     MIT License
+#-----------------------------------------------------------------------------
+
+"""
+Package Info
+
+Name: lib
+
+Description:
+- provide the module used for the infra monitor hub's frontend web host and the 
+backend data base handler .
+
+Modules inclided in the current package: 
+
+1. ConfigLoader.py: 
+- Provide API to load the not stand text format config file's data.
+
+"""
